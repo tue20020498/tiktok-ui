a1ff291983f0eb49b97fbc80d8640ebe30004eec
NguyenTrongTue <20020498@vnu.edu.vn>
2022-08-24 20:56:35 +0700

    Cấu hình Router/Layout

---
+++ src/page/Upload/index.js
@@ -0,0 +1,11 @@
+import Header from '~/components/Layout/DefaultLayout/Header';
+
+function Upload() {
+    return (
+        <div>
+            <h2>Upload Page</h2>
+        </div>
+    );
+}
+
+export default Upload;
---
+++ src/routes/index.js
@@ -0,0 +1,35 @@
+import Home from '~/page/Home';
+import Following from '~/page/Following';
+import Profile from '~/page/Profile';
+import Upload from '~/page/Upload';
+import { HeaderOnly } from '~/components/Layout';
+import Search from '~/page/Search';
+
+const publicRoutes = [
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '/following',
+        component: Following,
+    },
+    {
+        path: '/profile',
+        component: Profile,
+    },
+    {
+        path: '/upload',
+        component: Upload,
+        layout: HeaderOnly,
+    },
+    {
+        path: '/search',
+        component: Search,
+        layout: 0,
+    },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
